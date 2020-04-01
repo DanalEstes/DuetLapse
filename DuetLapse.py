c60b9294405d8608ae834242e270b36addf013ab
Danal Estes <danal.estes@gmail.com>
2020-04-01 02:45:02 -0500

    Even more work on options and help

--- DuetLapse.py
+++ DuetLapse.py
@@ -1,25 +1,17 @@
 #!/usr/bin/env python3
-# Python Script to take Time Lapse pictures during a print
-#   using images from a USB camera and a network
-#   interface to a Duet based printer. 
+# Python Script to take Time Lapse photographs during a print on 
+#   a Duet based 3D printer and convert them into a video. 
 #
 # Copyright (C) 2020 Danal Estes all rights reserved.
 # Released under The MIT License. Full text available via https://opensource.org/licenses/MIT
 #
-# Must run on the computer that owns the USB camera.  This is usually
-#   a Raspberry Pi.  It may be possible to adapt to other OS platforms. 
+# Implemented to run on Raspbian on a Raspberry Pi.  May be adaptable to other platforms. 
+# For USB or Pi camera, must run on a Raspberry Pi that is attached to camera.
+# For dlsr USB cameras, must run on a Raspberry Pi that owns the interface to the camera.  
+# For Webcam, the camera must be network reachable and via a defined URL for a still image.
+# 
+# The Duet printer must be RepRap firmware V2 or V3 and must be network reachable. 
 #
-# Requires network connection to Duet based printer running Duet/RepRap V2 or V3
-# Requires ffmpeg (sudo apt-get install ffmpeg)
-#
-#
-# This version of the script DOES NOT move the print head out of the way
-#   while taking pictures.  The print head will be in a random location. 
-#
-
-#import os
-#import numpy as np
-#import datetime
 
 import subprocess
 import sys
@@ -42,124 +34,192 @@ except ImportError:
 
 
 # Globals.
-zn = 0  # Z coordinate Now
-zo = 0  # Z coordiante Old
-frame = 0
-printerip = ''
-cameratype = ''
+zo = 0           # Z coordiante Old
+frame = 0        # Frame counter for file names
 printerState = 0 # State machine for print idle before print, printing, idle after print. 
 
-# parse command line arguments
-parser = argparse.ArgumentParser(description='Program to create time lapse video from camera pointed at Duet3D based printer.')
-parser.add_argument('-duet',type=str,nargs=1,help='Name or IP address of Duet printer.',required=True)
-parser.add_argument('-camera',type=str,nargs=1,choices=['usb','pi','web','dlsr'],default=['usb'])
-parser.add_argument('-interval',type=str,nargs=1,choices= ['nnn seconds', 'layer', 'pause'],default=['layer'])
-args=vars(parser.parse_args())
-duet     = args['duet'][0]
-camera   = args['camera'][0]
-interval = args['interval'][0]
+###########################
+# Methods begin here
+###########################
 
-# Warn user if we havent' implemented something yet. 
-if ((not 'usb' in camera) and (not 'pi' in camera)):
-    print('DuetLapse.py: error: Camera type '+camera+' not yet supported.')
-    exit(2)
 
-if ((not 'layer' in interval)):
-    print('DuetLapse.py: error: Interval type '+interval+' not yet supported.')
-    exit(2)
+def init():
+    # parse command line arguments
+    parser = argparse.ArgumentParser(description='Program to create time lapse video from camera pointed at Duet3D based printer.')
+    parser.add_argument('-duet',type=str,nargs=1,help='Name or IP address of Duet printer.',required=True)
+    parser.add_argument('-camera',type=str,nargs=1,choices=['usb','pi','web','dlsr'],default=['usb'])
+    parser.add_argument('-seconds',type=float,nargs=1,default=[0])
+    parser.add_argument('-detect',type=str,nargs=1,choices= ['layer', 'pause', 'none'],default=['layer'])
+    parser.add_argument('-pause',type=str,nargs=1,choices= ['yes', 'no'],default=['no'])
+    parser.add_argument('-movehead',type=float,nargs=2,default=[0.0,0.0])
+    args=vars(parser.parse_args())
+    global duet, camera, seconds, detect, pause, movehead
+    duet     = args['duet'][0]
+    camera   = args['camera'][0]
+    seconds  = args['seconds'][0]
+    detect   = args['detect'][0]
+    pause    = args['pause'][0]
+    movehead = args['movehead']
 
-# Check for requsite commands
-if ('usb' in camera):
-    if (20 > len(subprocess.check_output('whereis fswebcam', shell=True))):
-        print("Module 'fswebcam' is required. ")
-        print("Obtain via 'sudo apt install fswebcam'")
+    # Warn user if we havent' implemented something yet. 
+    if ((not 'usb' in camera) and (not 'pi' in camera)):
+        print('DuetLapse.py: error: Camera type '+camera+' not yet supported.')
         exit(2)
 
-if ('pi' in camera):
-    if (20 > len(subprocess.check_output('whereis raspistill', shell=True))):
-        print("Module 'raspistill' is required. ")
-        print("Obtain via 'sudo apt install raspistill'")
+    #if ((not 'layer' in interval)):
+    #    print('DuetLapse.py: error: Interval type '+interval+' not yet supported.')
+    #    exit(2)
+
+    # Inform regarding valid and invalid combinations
+    if ((seconds > 0) and (not 'none' in detect)):
+        print('Warning: -seconds '+str(seconds)+' and -detect '+detect+' will trigger on both.')
+        print('Specify "-detect none" with "-seconds" to trigger on seconds alone.')
+
+    if ((not movehead == [0.0,0.0]) and ((not 'yes' in pause) and (not 'pause' in detect))):
+        print('Invalid Combination: "-movehead {0:1.2f} {1:1.2f}" requires either "-pause yes" or "-detect pause".'.format(movehead[0],movehead[1]))
         exit(2)
 
-if (20 > len(subprocess.check_output('whereis ffmpeg', shell=True))):
-    print("Module 'ffmpeg' is required. ")
-    print("Obtain via 'sudo apt install ffmpeg'")
-    exit(2)
+    if (('yes' in pause) and ('pause' in detect)):
+        print('Invalid Combination: "-pause yes" causes this script to pause printer when')
+        print('other events are detected, and "-detect pause" requires the gcode on the printer')
+        print('contain its own pauses.  These are fundamentally incompatible.')
+        exit(2)
 
-# Get connected to the printer.
+    if ('pause' in detect):
+        print('************************************************************************************')
+        print('* Note "-detect pause" means that the G-Code on the printer already contains pauses,')
+        print('* and that this script will detect them, take a photo, and issue a resume.')
+        print('* Head position during those pauses is can be controlled by the pause.g macro ')
+        print('* on the duet, or by specifying "-movehead nnn nnn".')
+        print('*')
+        print('* If instead, it is desired that this script force the printer to pause with no')
+        print('* pauses in the gcode, specify either:')
+        print('* "-pause yes -detect layer" or "-pause yes -seconds nnn".')
+        print('************************************************************************************')
 
-print('Attempting to connect to printer at '+duet)
-printer = DWA.DuetWebAPI('http://'+duet)
-if (not printer.printerType()):
-    print('Device at '+duet+' either did not respond or is not a Duet V2 or V3 printer.')
-    exit(2)
-printer = DWA.DuetWebAPI('http://'+duet)
 
-print("Connected to a Duet V"+str(printer.printerType())+" printer at "+printer.baseURL())
+    if ('yes' in pause):
+        print('************************************************************************************')
+        print('* Note "-pause yes" means this script will pause the printer when the -detect or ')
+        print('* -seconds flags trigger.')
+        print('*')
+        print('* If instead, it is desired that this script detect pauses that are already in')
+        print('* in the gcode, specify:')
+        print('* "-detect pause"')
+        print('************************************************************************************')
 
-# Tell user options in use. 
-print()
-print("##################################")
-print("# Options in force for this run: #")
-print("# Camera   = {0:20s}#".format(camera))
-print("# Printer  = {0:20s}#".format(duet))
-print("# Interval = {0:20s}#".format(interval))
-print("##################################")
-print()
 
-# Clean up directory from past runs.
-try:
-    subprocess.call('rm -r /tmp/DuetLapse', shell=True)
-except: 
-    next
+    # Check for requsite commands
+    if ('usb' in camera):
+        if (20 > len(subprocess.check_output('whereis fswebcam', shell=True))):
+            print("Module 'fswebcam' is required. ")
+            print("Obtain via 'sudo apt install fswebcam'")
+            exit(2)
 
-subprocess.call('mkdir /tmp/DuetLapse', shell=True)
+    if ('pi' in camera):
+        if (20 > len(subprocess.check_output('whereis raspistill', shell=True))):
+            print("Module 'raspistill' is required. ")
+            print("Obtain via 'sudo apt install raspistill'")
+            exit(2)
 
-print('Waiting for print to start.')
+    if (20 > len(subprocess.check_output('whereis ffmpeg', shell=True))):
+        print("Module 'ffmpeg' is required. ")
+        print("Obtain via 'sudo apt install ffmpeg'")
+        exit(2)
+
+    # Get connected to the printer.
+
+    print('Attempting to connect to printer at '+duet)
+    global printer
+    printer = DWA.DuetWebAPI('http://'+duet)
+    if (not printer.printerType()):
+        print('Device at '+duet+' either did not respond or is not a Duet V2 or V3 printer.')
+        exit(2)
+    printer = DWA.DuetWebAPI('http://'+duet)
+
+    print("Connected to a Duet V"+str(printer.printerType())+" printer at "+printer.baseURL())
+
+    # Tell user options in use. 
+    print()
+    print("##################################")
+    print("# Options in force for this run: #")
+    print("# Camera   = {0:20s}#".format(camera))
+    print("# Printer  = {0:20s}#".format(duet))
+    print("# Seconds  = {0:20s}#".format(str(seconds)))
+    print("# Detect   = {0:20s}#".format(detect))
+    print("# Pause    = {0:20s}#".format(pause))
+    print("# Movehead = {0:6.2f} {1:6.2f}       #".format(movehead[0],movehead[1]))
+    print("##################################")
+    print()
+
+    # Clean up directory from past runs.  Be silent if it does not exist. 
+    try:
+        subprocess.call('rm -r /tmp/DuetLapse', shell=True)
+    except: 
+        next
+
+    subprocess.call('mkdir /tmp/DuetLapse', shell=True)
+
+    print('Waiting for print to start on printer '+duet)
+
+
+def onePhoto():
+    frame += 1
+    print("Capturing frame {0:5d} at Z {1:7.2f} .".format(int(np.around(frame)),zn ))
+    s="{0:08d}".format(int(np.around(frame)))
+    fn = '/tmp/DuetLapse/IMG'+s+'.jpeg'
+
+    if ('usb' in camera): 
+        cmd = 'fswebcam --quiet -d v4l2:/dev/video0 -i 0 -r 800x600 -p YUYV --no-banner '+fn
+    if ('pi' in camera): 
+        cmd = 'raspistill -o '+fn
+
+    subprocess.call(cmd, shell=True)
+
+def oneInterval():
+    if ('layer' in interval):
+        zn=printer.getCoords()['Z']
+        if (zn == zo):
+            return()
+        # Z changed, take a picture.
+        onePhoto()
+    zo = zn
+
+def postProcess():
+    print()
+    print("Now making {0:d} frames into a video at 10 frames per second.".format(int(np.around(frame))))
+    print("This can take a while...")
+    cmd='ffmpeg -r 10 -i /tmp/DuetLapse/IMG%08d.jpeg -vcodec libx264 -crf 25 -s 800x600 -pix_fmt yuv420p -y -v 8 ~/DuetLapse.mp4'
+    subprocess.call(cmd, shell=True)
+    print("Video processing complete.")
+    print("Video file is in home directory, named DuetLapse.mp4")
+    exit()    
+
+
+###########################
+# Main begins here
+###########################
+init()
+
+timeSleep = 1.01
+if (seconds > 0):
+    timeSleep = seconds
 
 while(1):
-    time.sleep(1.001) 
+    time.sleep(timeSleep) 
     status=printer.getStatus()
-    #print(printerState, status)
 
-    if (printerState == 0):  # Idle before print started. 
+    if (printerState == 0):     # Idle before print started. 
         if ('processing' in status):
             print('Print start sensed.')
             print('End of print will be sensed, and frames will be converted into video.')
             printerState = 1
-            continue
 
-    elif (printerState == 1):
+    elif (printerState == 1):   # Acutally printing
+        oneInterval()
         if ('idle' in status):
             printerState = 2
-            continue
 
-        if ('layer' in interval):
-            zn=printer.getCoords()['Z']
-            if (not zn == zo):
-                # Z changed, take a picture.
-                if ('usb' in camera): 
-                    s="{0:08d}".format(int(np.around(frame)))
-                    frame += 1
-                    cmd = 'fswebcam --quiet -d v4l2:/dev/video0 -i 0 -r 800x600 -p YUYV --no-banner '
-                    cmd += '/tmp/DuetLapse/IMG'+s+'.jpeg'
-                    print("Capturing frame {0:5d} at Z {1:7.2f} .".format(int(np.around(frame)),zn ))
-                    subprocess.call(cmd, shell=True)
-                if ('pi' in camera): 
-                    s="{0:08d}".format(int(np.around(frame)))
-                    frame += 1
-                    cmd = 'raspistill '
-                    cmd += ' -o /tmp/DuetLapse/IMG'+s+'.jpeg'
-                    print("Capturing frame {0:5d} at Z {1:7.2f} .".format(int(np.around(frame)),zn ))
-                    subprocess.call(cmd, shell=True)
-            zo = zn
+    elif (printerState == 2): 
+        postProcess()
 
-    elif (printerState == 2):
-        print()
-        print("Now making {0:d} frames into a video at 10 frames per second.".format(int(np.around(frame))))
-        print("This can take a while...")
-        cmd='ffmpeg -r 10 -i /tmp/DuetLapse/IMG%08d.jpeg -vcodec libx264 -crf 25 -s 800x600 -pix_fmt yuv420p -y -v 8 ~/DuetLapse.mp4'
-        subprocess.call(cmd, shell=True)
-        print("Video processing complete.")
-        print("Video file is in home directory, named DuetLapse.mp4")
-        exit()
